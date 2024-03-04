65d91cb1600eea3e9b2ef4a0d93a974a512095b6
Pékó Marcell <peko.marcell00@gmail.com>
2024-03-04 20:38:21 +0100

    battlefield component finished

--- src/app/battlefield/battlefield.component.ts
+++ src/app/battlefield/battlefield.component.ts
@@ -1,48 +1,73 @@
-import { Component, OnInit } from '@angular/core';
-import { DbService } from '../db.service';
-import { Kiscica } from 'src/types/cat-type';
+import { Component, OnInit } from "@angular/core";
+import { DbService } from "../db.service";
+import { Kiscica } from "src/types/cat-type";
 
 @Component({
-  selector: 'app-battlefield',
-  templateUrl: './battlefield.component.html',
-  styleUrls: ['./battlefield.component.css']
+  selector: "app-battlefield",
+  templateUrl: "./battlefield.component.html",
+  styleUrls: ["./battlefield.component.css"],
 })
 export class BattlefieldComponent implements OnInit {
+  firstCatData?: Kiscica;
+  secondCatData?: Kiscica;
+  isModalOpen: boolean = false;
+  winner?: string = "";
 
-  
-
-  firstCatData?:Kiscica
-  secondCatData?:Kiscica
-  isModalOpen:boolean = false
-
-  constructor(private db:DbService){}
+  constructor(private db: DbService) {}
 
   //const catFight = array[Math.floor(Math.random() * array.length)];
-  ngOnInit():any{
-    this.randomCatGenerator()
+  ngOnInit(): any {
+    this.randomCatGenerator();
   }
 
-  randomCatGenerator(){
-    this.db.getAllCats().subscribe(
-      (cats:any)=>{ 
+  randomCatGenerator() {
+    this.db.getAllCats().subscribe((cats: any) => {
+      this.firstCatData = cats[Math.floor(Math.random() * cats.length)];
 
-         this.firstCatData = cats[Math.floor(Math.random() * cats.length)]
+      this.secondCatData = cats[Math.floor(Math.random() * cats.length)];
 
-         this.secondCatData = cats[Math.floor(Math.random() * cats.length)]
-       
-       // console.log(this.firstCatData, this.secondCatData);
-              
+      // console.log(this.firstCatData, this.secondCatData);
+      let whoAttacksFirst: boolean = Math.random() <= 0.5;
+
+      if (whoAttacksFirst) {
+        let firstCase =
+          (this.firstCatData?.atk || 0) > (this.secondCatData?.def || 0);
+        let secondCase =
+          (this.secondCatData?.atk || 0) > (this.firstCatData?.def || 0);
+
+        if (firstCase) {
+          this.winner = this.firstCatData?.name;
+        } else if (secondCase) {
+          this.winner = this.secondCatData?.name;
+        } else {
+          this.winner = "Döntetlen, mindkét macska remekül harcolt!";
+        }
+      } else {
+        let firstCase =
+          (this.secondCatData?.atk || 0) > (this.firstCatData?.def || 0);
+        let secondCase =
+          (this.firstCatData?.atk || 0) > (this.secondCatData?.def || 0);
+
+        if (firstCase) {
+          this.winner = this.secondCatData?.name;
+        } else if (secondCase) {
+          this.winner = this.firstCatData?.name;
+        } else {
+          this.winner = "Döntetlen, mindkét macska remekül harcolt!";
+        }
       }
-    )
-      
-  }
-  
-  openModal(){
-    this.isModalOpen = true
+    });
   }
 
-  closeModal(){
-    this.isModalOpen = false
+  openModal() {
+    this.isModalOpen = true;
   }
 
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  newBattle() {
+    window.location.reload();
+  }
 }
